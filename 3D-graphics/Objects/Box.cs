fe6d1ad167c4b3417cb73a3b79ac3aa3c31e9367
JacekCzupyt <kuba.czupyt@gmail.com>
2021-06-12 18:32:03 +0200

    Refactor box to use triangles

--- 3D-graphics/Objects/Box.cs
+++ 3D-graphics/Objects/Box.cs
@@ -15,31 +15,43 @@ namespace _3D_graphics.Objects
             base(position, rotation, scale)
         { }
 
-        private IEnumerable<(Vector<double>, Vector<double>)> GetPoints()
+        private IEnumerable<Triangle> GetTriangles()
         {
-            List<(Vector<double>, Vector<double>)> list = new List<(Vector<double>, Vector<double>)>
+            List<Vector<double>> verticies = new List<Vector<double>>
             {
-                (Vector<double>.Build.DenseOfArray(new double[]{-1, 1, 1, 1}), Vector<double>.Build.DenseOfArray(new double[]{1, 1, 1, 1})),
-                (Vector<double>.Build.DenseOfArray(new double[]{-1, -1, 1, 1}), Vector<double>.Build.DenseOfArray(new double[]{1, -1, 1, 1})),
-                (Vector<double>.Build.DenseOfArray(new double[]{-1, 1, -1, 1}), Vector<double>.Build.DenseOfArray(new double[]{1, 1, -1, 1})),
-                (Vector<double>.Build.DenseOfArray(new double[]{-1, -1, -1, 1}), Vector<double>.Build.DenseOfArray(new double[]{1, -1, -1, 1})),
-
-                (Vector<double>.Build.DenseOfArray(new double[]{1, -1, 1, 1}), Vector<double>.Build.DenseOfArray(new double[]{1, 1, 1, 1})),
-                (Vector<double>.Build.DenseOfArray(new double[]{-1, -1, 1, 1}), Vector<double>.Build.DenseOfArray(new double[]{-1, 1, 1, 1})),
-                (Vector<double>.Build.DenseOfArray(new double[]{1, -1, -1, 1}), Vector<double>.Build.DenseOfArray(new double[]{1, 1, -1, 1})),
-                (Vector<double>.Build.DenseOfArray(new double[]{-1, -1, -1, 1}), Vector<double>.Build.DenseOfArray(new double[]{-1, 1, -1, 1})),
-
-                (Vector<double>.Build.DenseOfArray(new double[]{1, 1, -1, 1}), Vector<double>.Build.DenseOfArray(new double[]{1, 1, 1, 1})),
-                (Vector<double>.Build.DenseOfArray(new double[]{1, -1, -1, 1}), Vector<double>.Build.DenseOfArray(new double[]{1, -1, 1, 1})),
-                (Vector<double>.Build.DenseOfArray(new double[]{-1, 1, -1, 1}), Vector<double>.Build.DenseOfArray(new double[]{-1, 1, 1, 1})),
-                (Vector<double>.Build.DenseOfArray(new double[]{-1, -1, -1, 1}), Vector<double>.Build.DenseOfArray(new double[]{-1, -1, 1, 1})),
+                Vector<double>.Build.DenseOfArray(new double[]{-1, -1, -1, 1}),
+                Vector<double>.Build.DenseOfArray(new double[]{1, -1, -1, 1}),
+                Vector<double>.Build.DenseOfArray(new double[]{1, 1, -1, 1}),
+                Vector<double>.Build.DenseOfArray(new double[]{-1, 1, -1, 1}),
+                Vector<double>.Build.DenseOfArray(new double[]{-1, 1, 1, 1}),
+                Vector<double>.Build.DenseOfArray(new double[]{1, 1, 1, 1}),
+                Vector<double>.Build.DenseOfArray(new double[]{1, -1, 1, 1}),
+                Vector<double>.Build.DenseOfArray(new double[]{-1, -1, 1, 1})
             };
-            return list;
+
+            List<(int, int, int)> triangles = new List<(int, int, int)>
+            {
+                (0, 2, 1),//front
+                (0, 3, 2),
+                (1, 5, 6),//right
+                (1, 2, 5),
+                (6, 4, 7),//back
+                (6, 5, 4),
+                (7, 3, 0),//left
+                (7, 4, 3),
+                (2, 4, 5),//top
+                (2, 3, 4),
+                (0, 6, 7),//bottom
+                (0, 1, 6)
+            };
+            return triangles.Select(t => new Triangle(verticies[t.Item1], verticies[t.Item2], verticies[t.Item3]));
         }
 
         protected override IEnumerable<(Vector<double>, Vector<double>)> GetRawLines()
         {
-            return GetPoints().Select(l => (l.Item1.PointwiseMultiply(box_scale), l.Item2.PointwiseMultiply(box_scale)));
+            return GetTriangles()
+                .SelectMany(t => t.GetLines())
+                .Select(l => (l.Item1.PointwiseMultiply(box_scale), l.Item2.PointwiseMultiply(box_scale)));
         }
     }
 }
